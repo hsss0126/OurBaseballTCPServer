50943eee4a32ae1793a0e92365f866d23b9b6006
hsss0 <hsss0@DESKTOP-P73K90O>
2017-11-22 20:54:01 +0900

    채팅 TCP서버 / 대기실과 방에서 다중 채팅 가능 / 소켓제거 메소드는 아직 미구현

---
+++ src/tcp/chat/server/Server.java
@@ -0,0 +1,130 @@
+package tcp.chat.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Server {
+
+	private ServerSocket serverSocket;
+	private Socket socket;
+	
+	private Map<String, DataOutputStream> wClients = new HashMap<>();
+	private Map<String, Map<String, DataOutputStream>> rClients = new HashMap<>();
+	
+	public void setting() throws IOException {
+		Collections.synchronizedMap(wClients);
+		Collections.synchronizedMap(rClients);
+		
+		serverSocket = new ServerSocket(9999);
+		while(true) {
+			System.out.println("채팅 서버 대기중");
+			socket = serverSocket.accept();
+			
+			Receiver receiver = new Receiver(socket);
+			receiver.start();
+		}
+	}
+	
+	public void addWClients(String nickName, DataOutputStream out) {
+		wClients.put(nickName, out);
+		System.out.println(wClients.toString() + " 인원수: "+ wClients.size());
+	}
+	
+	public void removeWClients(String nickName) {
+		wClients.remove(nickName);
+		System.out.println("지워지나? 인원수: "+wClients.size());
+	}
+	
+	public void addRClients(String roomNo, String nickName, DataOutputStream out) {
+		if(rClients.containsKey(roomNo)) {
+			rClients.get(roomNo).put(nickName, out);
+			System.out.println(roomNo+"번 방 " + rClients.toString() + wClients.toString() + " 인원수: "+ wClients.size());
+		} else {
+			Map<String, DataOutputStream> roomClient = new HashMap<>();
+			roomClient.put(nickName, out);
+			rClients.put(roomNo, roomClient);
+			System.out.println(rClients.toString() + roomClient.toString() + " 인원수: "+ roomClient.size());
+		}
+	}
+	
+	public void removeRClients(String roomNo, String nickName) {
+		rClients.get(roomNo).remove(nickName);
+		if(rClients.get(roomNo).isEmpty()) {
+			rClients.remove(roomNo);	
+		}
+	}
+	
+	public void sendWMessage(String nickName, String msg) {
+		Iterator<String> it = wClients.keySet().iterator();
+		String key;
+		while(it.hasNext()) {
+			key = it.next();
+			try {
+				wClients.get(key).writeUTF(nickName +" : "+msg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void sendRMessage(String roomNo, String nickName, String msg) {
+		Iterator<String> it = rClients.get(roomNo).keySet().iterator();
+		String key;
+		while(it.hasNext()) {
+			key = it.next();
+			try {
+				rClients.get(roomNo).get(key).writeUTF(nickName +" : "+msg);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	class Receiver extends Thread {
+		private DataInputStream in;
+		private DataOutputStream out;
+		private String[] msgArray;
+		private String message;
+		
+		public Receiver(Socket socket) throws IOException {
+			
+			out = new DataOutputStream(socket.getOutputStream());
+			in = new DataInputStream(socket.getInputStream());
+			String msg = in.readUTF();
+			msgArray = msg.split("/");
+			if(msgArray[0].equals("0")) {
+				addWClients(msgArray[1], out);					//닉네임만 등록
+			} else {
+				addRClients(msgArray[0], msgArray[1], out);		//방 번호와 닉네임 등록
+			}
+		}
+
+		public void run() {
+			try {
+				while(in!=null) {
+					message = in.readUTF();
+					System.out.println(message);
+					msgArray = message.split("/");
+					if(msgArray[0].equals("0")) {
+						sendWMessage(msgArray[1], msgArray[2]);					//닉네임과 메세지 전송
+					} else {
+						sendRMessage(msgArray[0], msgArray[1], msgArray[2]);	//방번호와 닉네임과 메세지 전송
+					}
+				}
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		Server server = new Server();
+		server.setting();
+	}
+}
